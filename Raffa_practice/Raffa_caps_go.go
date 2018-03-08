30cdd84f389b5b8326688388e5c9ca8ccb2ea8d0
Raphael Gonzalez <Raphaelgonzaz@gmail.com>
2018-03-07 20:02:50 -0800

    IT WORKS IT FINALY WORKS!

--- Raffa_practice/Raffa_caps_go.go
+++ Raffa_practice/Raffa_caps_go.go
@@ -4,31 +4,28 @@ import "fmt"
 import "os"
 import "bufio"
 import s "strings"
-import sc "strconv"
+// import sc "strconv"
+// import b "bytes"
 
 func main() {
   reader := bufio.NewReader(os.Stdin)
   fmt.Print("Enter text: ")
   text, _ := reader.ReadString('\n')
-  fmt.Println(text)
+  // fmt.Println(text)
   fmt.Println(make_caps(text))
 }
 
-
 func make_caps(str string) string{
-  // a = len(str)
-  new_string := ""
+  new_string := []string {}
+
   for i, st := range str {
-    r_st := sc.QuoteRune(st)
+    // r_st := sc.QuoteRune(st)
     if i == 0 {
-      new_string += s.ToUpper(string(r_st))
+      new_string = append(new_string, s.ToUpper(string(st)))
     } else {
-      new_string += s.ToLower(string(r_st))
+      new_string = append(new_string, s.ToLower(string(st)))
     }
-    // else if st == "." {
-    //   new_string += s.ToUpper(st)
-    // }
   }
-  // return s.ToUpper(str)
-  return new_string
+  final_st := s.Join(new_string, "")
+  return final_st
 }

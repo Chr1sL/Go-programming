d3468d0a94db3444f18914f4d8ca3047a298c5dd
Raphael Gonzalez <Raphaelgonzaz@gmail.com>
2018-03-07 18:29:24 -0800

    x

--- Raffa_practice/Raffa_caps_go.go
+++ Raffa_practice/Raffa_caps_go.go
@@ -1,9 +1,10 @@
 package main
-
+// this is the capitalization corrector it doesn't know waht to do with  punctiation tho
 import "fmt"
 import "os"
 import "bufio"
 import s "strings"
+import sc "strconv"
 
 func main() {
   reader := bufio.NewReader(os.Stdin)
@@ -13,18 +14,20 @@ func main() {
   fmt.Println(make_caps(text))
 }
 
+
 func make_caps(str string) string{
+  // a = len(str)
   new_string := ""
   for i, st := range str {
+    r_st := sc.QuoteRune(st)
     if i == 0 {
-      new_string = new_string + s.ToUpper(st string)
+      new_string += s.ToUpper(string(r_st))
+    } else {
+      new_string += s.ToLower(string(r_st))
     }
-    elif == "." {
-      new_string = new_string + s.ToUpper(st string)
-    }
-    else {
-      new_string += st
-    }
+    // else if st == "." {
+    //   new_string += s.ToUpper(st)
+    // }
   }
   // return s.ToUpper(str)
   return new_string

5a45d61b06ae35b6588584a818b94f537758d1fe
RaffaGonzo <raphaelgonzalez1837@gmail.com>
2018-03-25 23:26:44 -0700

    now we only have to use the mailer function you wrote and then we have a functioning app. we can then add features like checking to make sure that students submit teacher emails and not their own and other stuff

--- pink_slips/handle_teacher.go
+++ pink_slips/handle_teacher.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"path/filepath"
+	"io/ioutil"
 	"os"
 )
 
@@ -18,7 +20,8 @@ func process_rsp(w http.ResponseWriter, r *http.Request)  {
 	// SEND THE STUDENT AN EMAIL THAT HAS THE TEACHER'S RESPONSE
 	prof_rsp := rsp{r.FormValue("rsp"), r.FormValue("id")}
 	prof_rsp.log_rsp()
-	
+	defer prof_rsp.prune() // forces the pruning/ deletion of irrelavent files to happen last so it doesnt happen too soon
+	// EMAIL STUDENT WITH RESPONSE OF THE TEACHER (YES/ NO)
 	fmt.Fprintf(w, "Thank you for your response it has been recorded and sent to your student!")
 }
 
@@ -26,17 +29,19 @@ type rsp struct {
 	reply, auth_id string
 }
 
-func (r *rsp)log_rsp(){
-	file_txt := r.auth_id + "txt"
-	f, err := os.OpenFile(file_txt, os.O_APPEND|os.O_WRONLY, 0600)
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-	text := r.reply
-	if _, err = f.WriteString("\n"+text); err != nil {
-		panic(err)
-	}
+func (r *rsp)log_rsp() {
+	file_txt := r.auth_id + ".txt"
+	path, _ := filepath.Abs("pink_slips/data")
+	data, _ := ioutil.ReadFile(filepath.Join(path, file_txt))
+	new_data := string(data) + "\n" + r.reply + "\n" + r.auth_id
+	log_file, _ := os.OpenFile(filepath.Join(path, "log.txt"), os.O_APPEND|os.O_WRONLY, 0600)
+
+	defer log_file.Close()
+	log_file.WriteString(new_data)
 }
 
-// make func prune()
+func (r *rsp) prune() (error, error) {
+	file_txt, file_html := r.auth_id + ".txt", r.auth_id + ".html"
+	path, _ := filepath.Abs("pink_slips/data")
+	return os.Remove(filepath.Join(path, file_txt)), os.Remove(filepath.Join(path, file_html))
+}
--- pink_slips/pink_slips.go
+++ pink_slips/pink_slips.go
@@ -2,9 +2,7 @@ package main
 // this helped a lot (link)
 import (
 	"fmt"
-	"html/template"
 	"io/ioutil"
-	"os"
 	"time"
 	"net/http"
 	"log"
@@ -22,12 +20,6 @@ func main() {
 	}
 	http.HandleFunc("/data", process) // handles student data
 	server.ListenAndServe() // does the server thing idk
-	fmt.Print("\n--SUCCESS--\n")
-	// keep --SUCCESS-- AT THE END
-
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHandler)
-	//http.HandleFunc("/save/", saveHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -41,7 +33,7 @@ func process(w http.ResponseWriter, r *http.Request) {
 	st_data := &Page{title, []byte(sbmt_data.prof_name + "\n" + sbmt_data.prof_addr + "\n" + sbmt_data.st_name + "\n" + sbmt_data.st_addr + "\n" + sbmt_data.skp_class + "\n" + sbmt_data.class_block+ "\n" + sbmt_data.evt_date + "\n" + sbmt_data.out_time)} // page being prepped for saving here is what the student has submitted and the teacher will be seeing
 	st_data.save() // this saves the data submitted to a txt and will make a n HTML file too
 	
-	// WE NEED TO SEND THIS DATA TO THE STUDENT AND THE TEACHER  (FOR THE STUDENT IT IS A CONFIRMATION)
+	// WE NEED TO SEND THIS DATA TO THE STUDENT AND THE TEACHER (FOR THE STUDENT IT IS A CONFIRMATION)
 
 	//reset(teacher_p.Title) // deletes the relevant files eventually should be put  in if statement or another  function ||| defer forces reset() to happen the end of the program
 	log.Print( title, "\n", string(st_data.Body))
@@ -62,45 +54,8 @@ func (p *Page) save() (error, error) { //no input but creates a file idk i coppi
 	file_txt := p.Title + ".txt" //info stored in txt
 	file_html := p.Title +".html"
 	body := strings.Replace(string(p.Body), "\n", "</p><p>", -1)
-	html_content := []byte("<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>"+p.Title+"</title></head><body><p>"+body+"</p><form action=\"http://192.168.1.19:8081/prof?rsp=&id=&thread=666\" method=\"post\" enctype=\"application/x-www-form-urlencoded\"><input type=\"radio\" name=\"rsp\" value=\"yes\" /><br><input type=\"radio\" name=\"rsp\" value=\"no\" /><br><table><tr><td>Computer Generated signature: </td><td><input type = \"password\" name = \"name\" required/></td></tr></table></form></body></html>") //file byte
+	html_content := []byte("<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>"+p.Title+"</title></head><body><p>"+body+"</p><form action=\"http://192.168.1.19:8081/prof?rsp=&id=&thread=666\" method=\"post\" enctype=\"application/x-www-form-urlencoded\"><input type=\"radio\" name=\"rsp\" value=\"yes\" required/>yes<br><input type=\"radio\" name=\"rsp\" value=\"no\" required/>no<br><table><tr><td>Computer Generated signature: </td><td><input type = \"text\" name = \"name\" required/></td></tr></table><input type=\"submit\" value=\"submit\" /></form></body></html>") //file byte
 
 	path, _ := filepath.Abs("pink_slips/data") // set path
 	return ioutil.WriteFile(filepath.Join(path, file_txt), p.Body, 0755), ioutil.WriteFile(filepath.Join(path,file_html), html_content, 0755)//writes both the record txt and the html file
-}
-
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
-}
-
-func reset(group string)  error { // this function will delete resolved stuff so after the teacher says yes or no it is resolved and all related docs are purged||| also "group" is just the naming strategy i was thinking of, like teacher name + student name or sm
-	file1 := group + ".txt"
-	//file2 := group + ".html" //this is the page that is created when the student submits and that the teacher sees
-	return os.Remove(file1) // os.Remove(file2) // does the removal
-}
-
-//https://golang.org/doc/articles/wiki/#tmp_6
-//https://golang.org/doc/articles/wiki/part3.go?m=text
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, _ := template.ParseFiles(tmpl + ".html")
-	t.Execute(w, p)
-}
-
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
-	renderTemplate(w, "view", p)
-}
-
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
-	p, err := loadPage(title)
-	if err != nil {
-		p = &Page{Title: title}
-	}
-	renderTemplate(w, "edit", p)
 }
